8c3ad1ca9e1b8affa48e9681d298cfab1f6b0648
Himel <hasibulislam8381@gmail.com>
2023-03-06 18:01:08 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+
+/*Data Types:
+undefined, null, boolean,string,symbol,number,object*/
+var a;
+var b = 2;
+
+a = 8;
+b=a;
+console.log(a);
+console.log(b);
+var str = "This is my first string\n";
+str+="This is my second string";
+console.log(str);
+var firstName = "Hasibul Islam";
+var len = firstName[6];
+var len1 = firstName[firstName.length-1];
+console.log(len);
+console.log(len1);
